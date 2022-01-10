970bce1830aeeb30fe2305f93d9aa048ff8db7e8
Thomas-Gabriel-Petcu <73488732+Thomas-Gabriel-Petcu@users.noreply.github.com>
2022-01-10 16:03:37 +0200

    Comitting rest of the api changes

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET','POST'])
 def signup():
-    return "<p>signup</p>"
+    if request.method == 'POST':
+        username = request.form.get('floatingUsername')
+        email = request.form.get('floatingEmail')
+        password1 = request.form.get('floatingPassword1')
+        password2 = request.form.get('floatingPassword2')
 
-@auth.route('/signin')
+        if len(username) <2:
+            print("Username was too short")
+        elif password1 != password2:
+            print("passwords did not match")
+        else:
+            print("Added user to database")
+
+    return render_template("signup.html")
+
+@auth.route('/signin', methods=['GET','POST'])
 def signin():
-    return "<p>signin</p>"
+    return render_template("login.html")
 
-@auth.route('/signout')
+@auth.route('/signout', methods=['GET','POST'])
 def signout():
     return "<p>signout</p>"
