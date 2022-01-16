d20678fa54b2f7e71e62af3f99daa74bbcfaa47e
NoTiX452 <theone.vali@yahoo.com>
2022-01-16 17:45:44 +0200

    Calculator&Profile BB

--- website/auth.py
+++ website/auth.py
@@ -1,47 +1,28 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request
+
 auth = Blueprint('auth',__name__)
-from website.functions import get_db_conn, db, create_user, get_user_credentials
 
 @auth.route('/signup', methods=['GET','POST'])
 def signup():
     if request.method == 'POST':
-        try:
-            #reference to request.form
-            form = request.form
-            #get database connection
-            conn = get_db_conn(db)
-            #pass form and connection as parameter to function for creating user
-            if form.get("password1") == form.get("password2"):
-                create_user(conn, form)
-            #close database connection
-            conn.close()
-        except Exception as e:
-            error = {
-                "error": f"Failed to create user. {e}"
-            }
-            return error
+        username = request.form.get('floatingUsername')
+        email = request.form.get('floatingEmail')
+        password1 = request.form.get('floatingPassword1')
+        password2 = request.form.get('floatingPassword2')
+
+        if len(username) <2:
+            print("Username was too short")
+        elif password1 != password2:
+            print("passwords did not match")
+        else:
+            print("Added user to database")
+
     return render_template("signup.html")
-
-
 
 @auth.route('/signin', methods=['GET','POST'])
 def signin():
-    if request.method == 'POST':
-        form = request.form
-        email = form.get("floatingEmail")
-        password = form.get("floatingPassword")
-        conn = get_db_conn(db)
-        user = get_user_credentials(conn, email)
-        if user[1] == password:
-            print("logged in")
-            return redirect(url_for('routes.account'))
-        else:
-            raise Exception("Failed to log in, passwords did not match")
-        conn.close()
     return render_template("login.html")
 
 @auth.route('/signout', methods=['GET','POST'])
 def signout():
     return "<p>signout</p>"
-
-
